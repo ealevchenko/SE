5231e464ac3035d019a4f8bee1f0ab71e0c4978e
eduard <Lev75.eduard@gmail.com>
2023-02-12 23:08:16 +0200

    add setup

---
+++ Script/setup.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox.Game.GameSystems;
+using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SETUP
+{
+    public sealed class Program : MyGridProgram
+    {
+        //void Main(string sArgument)
+        //{
+
+        //    List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+        //    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks);
+        //    Echo("blocks - " + blocks.Count());
+        //    foreach (IMyTerminalBlock bl in blocks) {
+        //        bl.CustomName = "NASTYA1-"+ bl.CustomName;
+        //        Echo("blocks - " + bl.CustomName); 
+        //    }
+        //}
+    }
+}
