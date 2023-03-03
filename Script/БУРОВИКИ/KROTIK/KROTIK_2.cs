6b9d5f9b25eb8373c5103d388c3157b4e0a02ca7
ealevchenko <eduard.levchenko@arcelormittal.com>
2023-03-03 16:55:05 +0200

    1

---
+++ Script/БУРОВИКИ/KROTIK/KROTIK_2.cs
@@ -0,0 +1,416 @@
+﻿using Sandbox.Definitions;
+using Sandbox.Game.GameSystems;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRageMath;
+
+namespace KROTIK_2
+{
+    public sealed class Program : MyGridProgram
+    {
+        // Название 
+        string NameObj = "[KROTIK2]";
+        string NameCockpit = "[KROTIK2]-Промышленный кокпит [LCD]";
+        string NameConnector = "[KROTIK2]-Коннектор парковка";
+        string NameLCDInfo = "[KROTIK2]-LCD-INFO";
+
+        static string tag_batterys_duty = "[batterys_duty]"; // дежурная батарея
+
+
+        LCD lcd_info;
+        Batterys bats;
+
+
+        BatteryBlock batterys;
+
+        static Program _scr;
+
+        public class PText
+        {
+            static public string GetPersent(double perse)
+            {
+                return " - " + Math.Round((perse * 100), 1) + "%";
+            }
+            static public string GetScalePersent(double perse, int scale)
+            {
+                string prog = "[";
+                for (int i = 0; i < Math.Round((perse * scale), 0); i++)
+                {
+                    prog += "|";
+                }
+                for (int i = 0; i < scale - Math.Round((perse * scale), 0); i++)
+                {
+                    prog += "'";
+                }
+                prog += "]" + GetPersent(perse);
+                return prog;
+            }
+            static public string GetCurrentOfMax(float cur, float max, string units)
+            {
+                return "[ " + cur + units + " / " + max + units + " ]";
+            }
+            static public string GetSign(int x, int y)
+            {
+                if (x == y) { return "="; }
+                if (x > y) { return ">"; }
+                if (x < y) { return "<"; }
+                return "";
+            }
+            static public string GetCountObj(int count, int count_on)
+            {
+                return count_on + GetSign(count_on, count) + count;
+            }
+            //------------------------------------------------------------------------
+            static public string GetCapacityTanks(double perse, float capacity)
+            {
+                return "[ " + Math.Round(((perse * capacity) / 1000000), 1) + "МЛ / " + Math.Round((capacity / 1000000), 1) + "МЛ ]";
+            }
+            static public string GetThrust(float value)
+            {
+                return Math.Round(value / 1000000, 1) + "МН";
+            }
+            static public string GetCurrentThrust(float to, float cur, float max)
+            {
+                return "[ " + GetThrust(to) + " / " + GetThrust(cur) + " / " + GetThrust(max) + " ]";
+            }
+            static public string GetCurrentThrust(float cur, float max)
+            {
+                return "[ " + GetThrust(cur) + " / " + GetThrust(max) + " ]";
+            }
+            static public string GetCountDetaliThrust(int count_a, int count_h, int count_i, int count_on_a, int count_on_h, int count_on_i)
+            {
+                string result = "[";
+                if (count_a > 0)
+                {
+                    result += "{А-" + count_on_a + "|" + count_a + "}";
+                }
+                if (count_h > 0)
+                {
+                    result += "{В-" + count_on_h + "|" + count_h + "}";
+                }
+                if (count_i > 0)
+                {
+                    result += "{И-" + count_on_i + "|" + count_i + "}";
+                }
+                result += "]";
+                return result;
+            }
+        }
+        public class BaseListTerminalBlock<T> where T : class
+        {
+            public List<T> list_obj = new List<T>();
+            public int Count { get { return list_obj.Count(); } }
+            public BaseListTerminalBlock(string name_obj)
+            {
+                _scr.GridTerminalSystem.GetBlocksOfType<T>(list_obj, r => ((IMyTerminalBlock)r).CustomName.Contains(name_obj));
+                _scr.Echo("Найдено" + typeof(T).Name + "[" + name_obj + "]: " + list_obj.Count());
+            }
+
+            public BaseListTerminalBlock(string name_obj, string tag)
+            {
+                _scr.GridTerminalSystem.GetBlocksOfType<T>(list_obj, r => ((IMyTerminalBlock)r).CustomName.Contains(name_obj));
+                if (!String.IsNullOrWhiteSpace(tag))
+                {
+                    list_obj = list_obj.Where(n => ((IMyTerminalBlock)n).CustomName.Contains(tag)).ToList();
+                }
+                _scr.Echo("Найдено" + typeof(T).Name + "[" + name_obj + "],[" + tag + "]: " + list_obj.Count());
+            }
+
+            // Команды включения\выключения
+            private void Off(List<T> list)
+            {
+                foreach (IMyTerminalBlock obj in list)
+                {
+                    obj.ApplyAction("OnOff_Off");
+                }
+            }
+            public void Off()
+            {
+                Off(list_obj);
+            }
+            private void OffOfTag(List<T> list, string tag)
+            {
+                foreach (IMyTerminalBlock obj in list)
+                {
+                    if (obj.CustomName.Contains(tag))
+                    {
+                        obj.ApplyAction("OnOff_Off");
+                    }
+                }
+            }
+            public void OffOfTag(string tag)
+            {
+                OffOfTag(list_obj, tag);
+            }
+            private void On(List<T> list)
+            {
+                foreach (IMyTerminalBlock obj in list)
+                {
+                    obj.ApplyAction("OnOff_On");
+                }
+            }
+            public void On()
+            {
+                On(list_obj);
+            }
+            private void OnOfTag(List<T> list, string tag)
+            {
+                foreach (IMyTerminalBlock obj in list)
+                {
+                    if (obj.CustomName.Contains(tag))
+                    {
+                        obj.ApplyAction("OnOff_On");
+                    }
+                }
+            }
+            public void OnOfTag(string tag)
+            {
+                OnOfTag(list_obj, tag);
+            }
+        }
+        public class BaseTerminalBlock<T> where T : class
+        {
+            public T obj;
+            public BaseTerminalBlock(string name)
+            {
+                obj = _scr.GridTerminalSystem.GetBlockWithName(name) as T;
+                _scr.Echo("block:[" + name + "]: " + ((obj != null) ? ("Ок") : ("not Block")));
+            }
+            public BaseTerminalBlock(T myobj)
+            {
+                obj = myobj;
+                _scr.Echo("block:[" + obj.ToString() + "]: " + ((obj != null) ? ("Ок") : ("not Block")));
+            }
+            // Команды включения\выключения
+            public void Off()
+            {
+                if (obj != null) ((IMyTerminalBlock)obj).ApplyAction("OnOff_Off");
+            }
+            public void On()
+            {
+                if (obj != null) ((IMyTerminalBlock)obj).ApplyAction("OnOff_On");
+            }
+        }
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            _scr = this;
+            lcd_info = new LCD(NameLCDInfo);
+            bats = new Batterys(NameObj);
+
+        }
+
+        public void Save()
+        {
+
+        }
+        // Переключить батареи
+        public void Main(string argument, UpdateType updateSource)
+        {
+            StringBuilder values_info = new StringBuilder();
+            bats.Logic(argument, updateSource);
+            //values_info.Append("Цель: " + target.ToString() + "\n");
+            //values_info.Append("Растояние: " + rem_con.GetDistance(target).ToString() + "\n");
+            //values_info.Append("Режим БУРЕНИЕ: " + (enable_drill ? "Вкл" : "Вык.") + "\n");
+            //values_info.Append("Режим АВТОПОЛЕТ: " + (enable_navigation ? "Вкл" : "Вык.") + "\n");
+            //values_info.Append("Info: " + message + "\n");
+            values_info.Append(bats.TextInfo());
+            lcd_info.OutText(values_info);
+        }
+
+        public class LCD : BaseTerminalBlock<IMyTextPanel>
+        {
+            public LCD(string name) : base(name)
+            {
+                if (base.obj != null)
+                {
+                    base.obj.SetValue("Content", (Int64)1);
+                }
+            }
+
+            public void OutText(StringBuilder values)
+            {
+                if (base.obj != null)
+                {
+                    base.obj.WriteText(values, false);
+                }
+            }
+            public void OutText(string text, bool append)
+            {
+                if (base.obj != null)
+                {
+                    base.obj.WriteText(text, append);
+                }
+            }
+        }
+        public class Batterys : BaseListTerminalBlock<IMyBatteryBlock>
+        {
+            public int count_work_batterys { get { return list_obj.Where(n => !((IMyTerminalBlock)n).CustomName.Contains(tag_batterys_duty)).Count(); } }
+            public bool charger = false;
+            public Batterys(string name_obj) : base(name_obj)
+            {
+                Init();
+            }
+            public Batterys(string name_obj, string tag) : base(name_obj, tag)
+            {
+                Init();
+            }
+            public void Init()
+            {
+                base.On();
+                charger = IsCharger();
+            }
+            public float MaxPower()
+            {
+                return base.list_obj.Select(b => b.MaxStoredPower).Sum();
+            }
+            public float CurrentPower()
+            {
+                return base.list_obj.Select(b => b.CurrentStoredPower).Sum();
+            }
+            public int CountCharger()
+            {
+                List<IMyBatteryBlock> res = base.list_obj.Where(b => ((IMyBatteryBlock)b).ChargeMode == ChargeMode.Recharge).ToList();
+                return res.Count();
+            }
+            public int CountAuto()
+            {
+                List<IMyBatteryBlock> res = base.list_obj.Where(b => ((IMyBatteryBlock)b).ChargeMode == ChargeMode.Recharge).ToList();
+                return res.Count();
+            }
+            public bool IsCharger()
+            {
+                int count_charger = CountCharger();
+                return count_work_batterys > 0 && count_charger > 0 && count_work_batterys == count_charger ? true : false;
+            }
+            public bool IsAuto()
+            {
+                int count_auto = CountAuto();
+                return Count > 0 && count_auto > 0 && Count == count_auto ? true : false;
+            }
+            public void Charger()
+            {
+                foreach (IMyBatteryBlock obj in base.list_obj)
+                {
+                    // проверка батарея дежурного режима
+                    if (!obj.CustomName.Contains(tag_batterys_duty))
+                    {
+                        obj.ChargeMode = ChargeMode.Recharge;
+                    }
+                }
+                charger = IsCharger();
+            }
+            public void Auto()
+            {
+                foreach (IMyBatteryBlock obj in base.list_obj)
+                {
+                    obj.ChargeMode = ChargeMode.Auto;
+                }
+                charger = IsCharger();
+            }
+            public void Logic(string argument, UpdateType updateSource)
+            {
+                switch (argument)
+                {
+                    case "bat_charger":
+                        Charger();
+                        break;
+                    case "bat_auto":
+                        Auto();
+                        break;
+                    case "bat_toggle":
+                        if (charger) { Auto(); } else { Charger(); }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (updateSource == UpdateType.Update10)
+                {
+
+                }
+
+            }
+
+            public string TextInfo()
+            {
+                StringBuilder values = new StringBuilder();
+                //БАТАРЕЯ: [10 - 10] [0.0MW / 0.0MW]
+                //|- ЗАР:  [''''''''''''''''''''''''']-0%
+                //| -ДЕЖ: -[''''''''''''''''''''''''']-0%
+                values.Append("БАТАРЕЯ: [" + Count + "] [А-" + CountAuto() + " З-" + CountCharger() + "]" + PText.GetCurrentOfMax(CurrentPower(), MaxPower(), "кW") + "\n");
+                values.Append("|- ЗАР:  " + PText.GetScalePersent(CurrentPower() / MaxPower(), 20) + "\n");
+                return values.ToString();
+            }
+        }
+        public class BatteryBlock
+        {
+            List<IMyBatteryBlock> batterylist = new List<IMyBatteryBlock>();
+            public int Count { get { return batterylist.Count(); } }
+            public BatteryBlock(string name_group)
+            {
+
+                _scr.GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batterylist, r => r.CustomName.Contains(name_group));
+                _scr.Echo("batterylist: " + ((batterylist != null && batterylist.Count() > 0) ? ("Ок") : ("not batterylist")));
+            }
+
+            public string GetMode()
+            {
+                string result = "";
+                foreach (IMyBatteryBlock batt in batterylist)
+                {
+                    result = batt.ChargeMode.ToString();
+                }
+                return result;
+            }
+
+            public string GetModeOfText()
+            {
+                string result = "БАТ:";
+                foreach (IMyBatteryBlock batt in batterylist)
+                {
+                    switch (batt.ChargeMode)
+                    {
+
+                        case ChargeMode.Auto:
+                            {
+                                result += "А|";
+                                break;
+                            };
+                        case ChargeMode.Recharge:
+                            {
+                                result += "З|";
+                                break;
+                            };
+                        case ChargeMode.Discharge:
+                            {
+                                result += "Р|";
+                                break;
+                            };
+                    }
+                }
+                return result;
+            }
+            public void Charger()
+            {
+                foreach (IMyBatteryBlock batt in batterylist)
+                {
+                    batt.ChargeMode = ChargeMode.Recharge;
+                }
+            }
+            public void Auto()
+            {
+                foreach (IMyBatteryBlock batt in batterylist)
+                {
+                    batt.ChargeMode = ChargeMode.Auto;
+                }
+            }
+        }
+    }
+}
